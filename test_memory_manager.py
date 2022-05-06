f3c256389a5e5924e4fd98d70ffc2b00243fa7cf
Hisao <jaimehisao2011@gmail.com>
2022-05-05 23:22:41 -0500

    added memory management testing

---
+++ test_memory_manager.py
@@ -0,0 +1,34 @@
+import memory_manager
+
+memory_manager = memory_manager.MemoryManager()
+
+
+def test_assign_new_int_address_in_interval():
+    assigned_address = memory_manager.assign_new_int_address()
+    assert assigned_address >= 10000
+    assert assigned_address <= 19999
+
+
+def test_assign_new_float():
+    assigned_address = memory_manager.assign_new_float()
+    assert assigned_address >= 20000
+    assert assigned_address <= 29999
+
+
+def test_assign_new_char():
+    assigned_address = memory_manager.assign_new_char()
+    assert assigned_address >= 30000
+    assert assigned_address <= 39000
+
+
+def test_assign_new_temp():
+    assigned_address = memory_manager.assign_new_temp()
+    assert assigned_address >= 40000
+    assert assigned_address <= 59000
+
+
+def test_get_variable_from_address():
+    assert memory_manager.get_variable_from_address(10000) == "int"
+    assert memory_manager.get_variable_from_address(20000) == "float"
+    assert memory_manager.get_variable_from_address(30000) == "char"
+    assert memory_manager.get_variable_from_address(40000) == "temp"
