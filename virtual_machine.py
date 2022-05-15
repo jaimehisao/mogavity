8c46a1dcaba119917d872bdab1cb529a3196a3aa
Hisao <jaimehisao2011@gmail.com>
2022-05-14 21:14:02 -0500

    virt mach

---
+++ virtual_machine.py
@@ -0,0 +1,17 @@
+"""
+virtual_machine.py - Virtual Machine for the Mogavity Compiler
+The VM for the Mogavity compiler is an efficient machine that helps us run the intermediate code
+generated during compilation.
+"""
+
+
+def send_memory_to_sleep():
+    pass
+
+
+def wake_up_memory():
+    pass
+
+
+def allocate_memory(ints: int, floats: int, chars: int):
+    pass
